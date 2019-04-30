2a5073cf854f1715382b87e57a92c54e7a1c8eed
mircurin <rmp8@yandex.ru>
2019-04-30 15:55:27 +0300

    les2

--- les2/index.php
+++ les2/index.php
@@ -1,13 +1,41 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>les2</title>
 </head>
 <body>
+    <?php
+        $a = 5;
+        $b = 8;
+        if($a > 0 & $b > 0) {
+            echo $a - $b;
+        }
+    ?>
+    <?php
+        $a = -5;
+        $b = -8;
+        if($a < 0 & $b < 0) {
+            echo $a * $b;
+        }
+    ?>
 
+    <?php
+        $a = -5;
+        $b = -8;
+        if(($a < 0 || $a > 0) & ($b < 0 || $b >0)) {
+            echo $a + $b;
+        }
+    ?>
+
+    <?php
+        $a = 5;
+        switch (true){
+            
+        }
+    ?>
 </body>
 </html>
